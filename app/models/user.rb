fcb1184beb4c20e28a699bdc75f561abee8eb3cc
Sarah Garlock <garlock.sarah10@gmail.com>
2023-08-08 11:20:13 -0600

    add user model and spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates_presence_of :password_digest
-  # password_confirmation?
-  # api_key?
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+
+  # before_create :generate_api_key
+
+  private
+
+  # def generate_api_key
+  #   loop do
+  #     self.api_key = SecureRandom.base58(24)
+  #     break unless User.exists?(api_key: api_key)
+  #   end
+  # end
 end
