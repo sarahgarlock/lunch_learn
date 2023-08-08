b842f7d9c8afad3b495d316a6b25dbcdd7f4b2b0
Sarah Garlock <garlock.sarah10@gmail.com>
2023-08-08 14:34:38 -0600

    start on get favorites spec

---
+++ spec/requests/api/v1/get_favorite_request_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe "Get Favorites API", type: :request do
+  describe "happy path" do
+    it "returns a list of favorites for a specific user", :vcr do
+  end
+end
