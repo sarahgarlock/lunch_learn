14ac4f1c300c28005126e1bd6d036a708b3c3321
Sarah Garlock <garlock.sarah10@gmail.com>
2023-08-08 14:31:58 -0600

    add route for favorites

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       resources :learning_resources, only: [:index]
       resources :users, only: [:create]
       resources :sessions, only: [:create]
+      resources :favorites, only: [:create]
     end
   end
 end
