9ac263cb38166b5f933ff4687d31d46a60838df8
Sarah Garlock <garlock.sarah10@gmail.com>
2023-08-08 15:11:40 -0600

    add index for favorites

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::FavoritesController < ApplicationController
+  def index
+    user = User.find_by(api_key: params[:api_key])
+    if user
+      favorites = user.favorites
+      render json: FavoriteSerializer.new(favorites)
+    else
+      render json: { error: "Invalid API Key" }, status: :bad_request
+    end
+  end
+
   def create
     user = User.find_by(api_key: params[:api_key])
     if user
       favorite = user.favorites.create(favorite_params)
-      render json: { message: "Successfully added #{favorite_params[:recipe_title]} to your favorites", favorite: favorite }, status: :created
+      render json: { message: "Successfully added #{favorite_params[:recipe_title]} to your favorites"}, status: :created
     else
       render json: { error: "Invalid API Key" }, status: :bad_request
     end
