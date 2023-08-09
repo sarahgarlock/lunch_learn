409dfc4b8c91f992eaf038400295f7611f15417d
Sarah Garlock <garlock.sarah10@gmail.com>
2023-08-08 20:56:44 -0600

    last refactor, I swear

--- spec/poros/learning_resource_spec.rb
+++ spec/poros/learning_resource_spec.rb
@@ -8,8 +8,16 @@ RSpec.describe LearningResource do
       :video => "https://www.youtube.com/watch?v=0V5BwTruJv8"
     }
 
-    resource = LearningResource.new("India", "https://www.flickr.com/photos/144365873@N07/albums/72157661556391756", "https://www.youtube.com/watch?v=0V5BwTruJv8")
+    country = data[:country]
+    photo = data[:photo]
+    video = data[:video]
 
+    resource = LearningResource.new(country, photo, video)
+    
+    expect(resource).to be_a(LearningResource)
+    expect(resource.country).to eq("India")
+    expect(resource.photo).to eq("https://www.flickr.com/photos/144365873@N07/albums/72157661556391756")
+    expect(resource.video).to eq("https://www.youtube.com/watch?v=0V5BwTruJv8")
 
   end
 end
